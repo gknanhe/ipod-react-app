bc17367816995ef8b3ae344d6682aacd132db148
Ganesh Nanhe <74034986+gknanhe@users.noreply.github.com>
2023-07-03 09:13:41 +0000

    Update package.json and index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom";
 import "./assets/css/index.css";
 import App from "./components/stateful/App";
 
 // Rendering the React App into the HTML DOM Element "root"
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
